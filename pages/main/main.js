4d882bd0d8f42d97dd2bac1ee5b35fe599cb39a9
chenyu <1213142545@qq.com>
2020-02-18 21:39:58 +0800

    Initial Commit

---
+++ pages/main/main.js
@@ -0,0 +1,172 @@
+// pages/main/main.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    items: [{
+      ID: 1001,
+      title: "简单小任务，在家躺赚钱",
+      pay: "300元 / 天",
+      location: "武侯区",
+      duration: "长期",
+      dailyknots: "日结",
+      popular: "人气"
+    }, {
+        ID: 1002,
+        title: "简单小任务，在家躺赚钱",
+        pay: "300元 / 天",
+        location: "武侯区",
+        duration: "长期",
+        dailyknots: "日结",
+        popular: "人气"
+      }, {
+        ID: 1003,
+        title: "简单小任务，在家躺赚钱",
+        pay: "300元 / 天",
+        location: "武侯区",
+        duration: "长期",
+        dailyknots: "日结",
+        popular: "人气"
+      }, {
+        ID: 1003,
+        title: "简单小任务，在家躺赚钱",
+        pay: "300元 / 天",
+        location: "武侯区",
+        duration: "长期",
+        dailyknots: "日结",
+        popular: "人气"
+      }, {
+        ID: 1003,
+        title: "简单小任务，在家躺赚钱",
+        pay: "300元 / 天",
+        location: "武侯区",
+        duration: "长期",
+        dailyknots: "日结",
+        popular: "人气"
+      }, {
+        ID: 1003,
+        title: "简单小任务，在家躺赚钱",
+        pay: "300元 / 天",
+        location: "武侯区",
+        duration: "长期",
+        dailyknots: "日结",
+        popular: "人气"
+      }, {
+        ID: 1003,
+        title: "简单小任务，在家躺赚钱",
+        pay: "300元 / 天",
+        location: "武侯区",
+        duration: "长期",
+        dailyknots: "日结",
+        popular: "人气"
+      }, {
+        ID: 1003,
+        title: "简单小任务，在家躺赚钱",
+        pay: "300元 / 天",
+        location: "武侯区",
+        duration: "长期",
+        dailyknots: "日结",
+        popular: "人气"
+      }, {
+        ID: 1003,
+        title: "简单小任务，在家躺赚钱",
+        pay: "300元 / 天",
+        location: "武侯区",
+        duration: "长期",
+        dailyknots: "日结",
+        popular: "人气"
+      }, {
+        ID: 1003,
+        title: "简单小任务，在家躺赚钱",
+        pay: "300元 / 天",
+        location: "武侯区",
+        duration: "长期",
+        dailyknots: "日结",
+        popular: "人气"
+      }, {
+        ID: 1003,
+        title: "简单小任务，在家躺赚钱",
+        pay: "300元 / 天",
+        location: "武侯区",
+        duration: "长期",
+        dailyknots: "日结",
+        popular: "人气"
+      }, {
+        ID: 1003,
+        title: "简单小任务，在家躺赚钱",
+        pay: "300元 / 天",
+        location: "武侯区",
+        duration: "长期",
+        dailyknots: "日结",
+        popular: "人气"
+      }, {
+        ID: 1003,
+        title: "简单小任务，在家躺赚钱",
+        pay: "300元 / 天",
+        location: "武侯区",
+        duration: "长期",
+        dailyknots: "日结",
+        popular: "人气"
+      },]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    // this.setData({
+    //   username: options.username
+    // })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
